2fd223a020a96cf20e50bc45c1aec58d9ad83ee7
Benicere <augustomantero06@gmail.com>
2024-01-19 16:45:52 -0300

    contacto + footer + nav y inicio home responsive

---
+++ index.js
@@ -0,0 +1,18 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    var menuToggle = document.getElementById('menu-toggle');
+    var menuEmergente = document.querySelector('.menu-emergente');
+
+    menuToggle.addEventListener('change', function () {
+      menuEmergente.style.display = this.checked ? 'block' : 'none';
+    });
+
+    // Cierra el menú emergente si se hace clic en un enlace
+    menuEmergente.addEventListener('click', function (event) {
+      if (event.target.tagName === 'A') {
+        menuToggle.checked = false;
+        menuEmergente.style.display = 'none';
+      }
+    });
+  });
+</script>
