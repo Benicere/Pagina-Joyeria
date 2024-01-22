fb80b38bd4d504e053ac449a6df2d3c94214d8d9
Tobias <tobyajras@gmail.com>
2024-01-21 23:18:08 -0300

    archivos javascript comentados

--- contacto.js
+++ contacto.js
@@ -1,24 +1,24 @@
-const btn = document.getElementById("button");
+// const btn = document.getElementById("button");
 
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
 
-    btn.value = "Enviando...";
-    btn.disabled = true;
+//     btn.value = "Enviando...";
+//     btn.disabled = true;
 
-    const serviceID = "default_service";
-    const templateID = "template_w7yxex8";
+//     const serviceID = "default_service";
+//     const templateID = "template_w7yxex8";
 
-    emailjs.sendForm(serviceID, templateID, this).then(
-      () => {
-        btn.value = "Correo enviado con éxito!";
-        this.reset();
-      },
-      (err) => {
-        btn.value = "Enviar";
-        btn.disabled = false;
-      }
-    );
-  });
+//     emailjs.sendForm(serviceID, templateID, this).then(
+//       () => {
+//         btn.value = "Correo enviado con éxito!";
+//         this.reset();
+//       },
+//       (err) => {
+//         btn.value = "Enviar";
+//         btn.disabled = false;
+//       }
+//     );
+//   });
