580e2ba2823e42f01e2a8cffd39c1f1c5a7d633f
Tobias <tobyajras@gmail.com>
2024-01-21 23:26:48 -0300

    javascript descomentado

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var menuToggle = document.getElementById("menu-toggle");
-//   var menuEmergente = document.querySelector(".menu-emergente");
+document.addEventListener("DOMContentLoaded", function () {
+  var menuToggle = document.getElementById("menu-toggle");
+  var menuEmergente = document.querySelector(".menu-emergente");
 
-//   menuToggle.addEventListener("change", function () {
-//     menuEmergente.style.display = this.checked ? "block" : "none";
-//   });
+  menuToggle.addEventListener("change", function () {
+    menuEmergente.style.display = this.checked ? "block" : "none";
+  });
 
-//   // Cierra el menú emergente si se hace clic en un enlace
-//   menuEmergente.addEventListener("click", function (event) {
-//     if (event.target.tagName === "A") {
-//       menuToggle.checked = false;
-//       menuEmergente.style.display = "none";
-//     }
-//   });
-// });
+  // Cierra el menú emergente si se hace clic en un enlace
+  menuEmergente.addEventListener("click", function (event) {
+    if (event.target.tagName === "A") {
+      menuToggle.checked = false;
+      menuEmergente.style.display = "none";
+    }
+  });
+});
